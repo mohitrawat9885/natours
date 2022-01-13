903254b70c3d548f5393b0c84e13538bc869d539
Mohit Rawat <mohitrawat9885@gmail.com>
2022-01-13 18:36:48 +0530

    Message bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -65,4 +65,4 @@ if (bookBtn)
   });
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
+if (alert) showAlert('success', alertMessage, 20);
